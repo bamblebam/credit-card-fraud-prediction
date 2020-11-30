18a224e3f58ebb31e6245bc27d0a9716a95653ac
bamblebam <burhanizrangwala@gmail.com>
2020-11-30 20:37:02 +0530

    plots

--- visualisation.py
+++ visualisation.py
@@ -12,3 +12,38 @@ from sklearn.svm import OneClassSVM
 # %%
 RANDOM_SEED = 42
 LABELS = ["normal", "fraud"]
+
+# %%
+dataset = pd.read_csv('./dataset/creditcard.csv')
+dataset.head()
+# %%
+dataset.info()
+# %%
+classes = pd.value_counts(dataset['Class'], sort=True)
+classes.plot(kind='bar')
+plt.title("Transaction Distribution")
+plt.xlabel("Class")
+plt.ylabel("Frequency")
+plt.xticks(range(2), LABELS)
+# %%
+fraud = dataset[dataset["Class"] == 1]
+normal = dataset[dataset["Class"] == 0]
+print(fraud.shape, normal.shape)
+
+# %%
+fraud.Amount.describe()
+# %%
+normal.Amount.describe()
+# %%
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+f.suptitle("Transaction Amount")
+bins = 50
+ax1.hist(fraud.Amount, bins=bins)
+ax1.set_title("Fraud")
+ax2.hist(normal.Amount, bins=bins)
+ax2.set_title("Normal")
+plt.xlabel("Amount")
+plt.ylabel("Frequency")
+plt.yscale('log')
+plt.show()
+# %%
